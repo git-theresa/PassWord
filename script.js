337e06a5b743059b26085c5c705138ddd2245527
Theresa E <t.eatherly@gmail.com>
2020-02-18 17:54:47 -0600

    all new

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Variables
+var letters = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
+      ,"r","s","t","u","v","w","x","y","z");
+
+var  characters = ("!", "@", "#", "$", "%", "&", "*");
+// random number
+
+Math.floor(math.radom()*10);
+
+// random letter
+function Letters( ) {
+  var letter = letters[Math.floor(Math.random()*letters.length)];
+    return letter
+  
+  }
+  
+  
+  
