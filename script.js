70f8027a9c1a8a15e1bef7094db595bff36cbee8
Theresa E <t.eatherly@gmail.com>
2020-02-22 13:43:26 -0600

    changes to file path

--- script.js
+++ script.js
@@ -1,4 +1,66 @@
 
+// Dom elements
+var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
+// // // Event button
+generateBtn.addEventListener("click", () =>{
+    var length = prompt
+    ("Choose your password length. At least 8  no more than 128 characters");
+    var lowerCase = confirm
+    ("Do you want Lowercase?");
+    var upperCase = confirm
+    ("Do you want Uppercase?");
+    var numeric = confirm
+    ("Do you want Numbers?");
+    var special = confirm
+    ("Do you want Special Characters?");
+});
+// Functions, CharCode (http://net-comber.com/charset.html)
+function LowerCase (){
+return String.fromCharCode (Math.floor(Math.random() * 26) + 97);
+}
+console.log(LowerCase());
+function UpperCase (){
+    return String.fromCharCode (Math.floor(Math.random() * 26) + 65);
+    }
+console.log(UpperCase());
+function NumberCase (){
+    return String.fromCharCode (Math.floor(Math.random() * 10) + 48);
+    }
+console.log(NumberCase());
+function Special (){
+    return String.fromCharCode (Math.floor(Math.random() * 15) + 33);
+    }
+console.log(Special());
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// Variables
+var letters = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
+      ,"r","s","t","u","v","w","x","y","z");
+
+var  characters = ("!", "@", "#", "$", "%", "&", "*");
+// random number
+
+
+
+
+  
+  
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -62,63 +124,4 @@ console.log(characters());
 
 
 
-// Write password to the #password input
- function writePassword(){
-   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-generateBtn.addEventListener("click", writePassword);
-  passwordText.value = password;
- }
 
-console.log(writePassword);
-
-
-  
-
-// Variables
-var lettersUpper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-console.log(lettersUpper);
-console.log(lettersUpper.length);
-
-var lettersLower= "abcdefghijklmnopqrstuvwxyz";
-console.log(lettersLower);
-console.log(lettersLower.length);
-
-var  characters = "!@#$%&*";
-console.log(characters);
-console.log(characters.length);
-
-var number= "0123456789"
-console.log(number);
-console.log (number.length);
-
-("Do you want Lowercase?");
-var upperCase = console.log (upperCase);
-("Do you want Uppercase?");
-var numeric = console.log (numeric);
-("Do you want Numbers?");
-var special = console.log (special);
-("Do you want Special Characters?");
-var length = console.log (length);
-("Choose your password length? At least 8 no more than 128 characters");
-var L = "would you like lowercase letters?"
-var N = "would you like numbers?"
-var C = "would you like Special Characters?"
-for (var i=0; i= Number; i++) {
-  var password = Math.floor(Math.random() * lettersLower.legnth  );
-  document.getElementById("btn").addEventListener("click", writePassword());
-  
-    document.getElementById("btn").alert = "Password";
-  }
-
-
-function makeid(length) {
-  var result = '';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-console.log(makeid(10));
